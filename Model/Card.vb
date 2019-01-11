923c5b9f08b74e6b41fd2e50c7a26231d75cad33
woodfor <woodforlol@gmail.com>
2019-01-12 01:19:03 +0800

    Revert "data structure"
    This reverts commit f169b092fce74ef3da651b48e93bf349de990952.

--- Model/Card.vb
+++ Model/Card.vb
@@ -1,39 +1,127 @@
-Imports System
-Imports System.Collections.Generic
-Imports System.ComponentModel.DataAnnotations
-Imports System.ComponentModel.DataAnnotations.Schema
-Imports System.Data.Entity.Spatial
+﻿Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Public Class Card
+    Private _cardNumber As String
+    Public Property cardNumber() As String
+        Get
+            Return _cardNumber
+        End Get
+        Set(ByVal value As String)
+            _cardNumber = value
+        End Set
+    End Property
+    Private _ID As String
+    Public Property ID() As String
+        Get
+            Return _ID
+        End Get
+        Set(ByVal value As String)
+            _ID = value
+        End Set
+    End Property
+    Private _status As String
+    Public Property status() As String
+        Get
+            Return _status
+        End Get
+        Set(ByVal value As String)
+            _status = value
+        End Set
+    End Property
 
-<Table("Card")>
-Partial Public Class Card
-    Public Sub New()
-        Trans = New HashSet(Of Trans)()
-    End Sub
+    '图书所在类别
 
-    <Key>
-    <StringLength(50)>
-    Public Property card_id As String
+    Private _bonus As Integer
+    Public Property bonus() As Integer
+        Get
+            Return _bonus
+        End Get
+        Set(ByVal value As Integer)
+            _bonus = value
+        End Set
+    End Property
 
-    <Required>
-    <StringLength(20)>
-    Public Property user_name As String
+    Private _memberLevel As String
+    Public Property memberLevel() As String
+        Get
+            Return _memberLevel
+        End Get
+        Set(ByVal value As String)
+            _memberLevel = value
+        End Set
+    End Property
 
-    <Required>
-    <StringLength(50)>
-    Public Property open_id As String
+    '图书价格
 
-    Public Property balance As Decimal
+    'Private _price As Single
+    'Public Property price() As Single
+    '    Get
+    '        Return _price
+    '    End Get
+    '    Set(ByVal value As Single)
+    '        _price = value
+    '    End Set
+    'End Property
 
-    Public Property store_id As Integer
+    '库存
 
-    <Required>
-    <StringLength(20)>
-    Public Property customer_phone As String
+    'Private _count As Integer
+    'Public Property count() As Integer
+    '    Get
+    '        Return _count
+    '    End Get
+    '    Set(ByVal value As Integer)
+    '        _count = value
+    '    End Set
+    'End Property
 
-    <Column(TypeName:="date")>
-    Public Property customer_datbirth As Date
+    '出版社
 
-    Public Overridable Property Store As Store
+    'Private _publish As String
+    'Public Property publish() As String
+    '    Get
+    '        Return _publish
+    '    End Get
+    '    Set(ByVal value As String)
+    '        _publish = value
+    '    End Set
+    'End Property
 
-    Public Overridable Property Trans As ICollection(Of Trans)
+    '图书条形码
+
+    'Private _barcode As String
+    'Public Property barcode() As String
+    '    Get
+    '        Return _barcode
+    '    End Get
+    '    Set(ByVal value As String)
+    '        _barcode = value
+    '    End Set
+    'End Property
+
+    '图书图片
+
+    'Private _bookPhoto As Byte()
+    'Public Property bookPhoto() As Byte()
+    '    Get
+    '        Return _bookPhoto
+    '    End Get
+    '    Set(ByVal value As Byte())
+    '        _bookPhoto = value
+    '    End Set
+    'End Property
+
+    '出版日期
+
+    'Private _publishDate As DateTime
+    'Public Property publishDate() As DateTime
+    '    Get
+    '        Return _publishDate
+    '    End Get
+    '    Set(ByVal value As DateTime)
+    '        _publishDate = value
+    '    End Set
+    'End Property
+
 End Class
